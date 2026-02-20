454fb65f6e3a277bff1234029a64c3d99b21e36c
Davy <68999755+dcoudenis@users.noreply.github.com>
2026-02-20 09:29:07 +0100

    Update configuration and README.md

--- get_activities.py
+++ get_activities.py
@@ -13,8 +13,8 @@ import sys
 ### CONFIG ###
 output_style = "summary"  # options are "summary" or "detailed"
 first_activity_date = "2026-01-01"  # format YYYY-MM-DD - this is the date after which you want to pull activities. Change as needed.
-FTP = 250
-strava_api_url = "https://www.strava.com/api/v3"
+FTP = 250 # this is your Functional Threshold Power in watts - change as needed for accurate FTP% calculations. If you don't have an FTP, you can set this to 1 to avoid errors, but the FTP% values will not be meaningful.
+strava_api_url = "https://www.strava.com/api/v3" # this is the base URL for the Strava API - it should not need to be changed, but you can change it if you want to use a different version of the API or a different endpoint.
 ### END OF CONFIG ###
 
 def eprint(*args, **kwargs):
